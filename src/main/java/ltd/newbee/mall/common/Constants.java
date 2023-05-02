3a637712d0844bdc6871ec24d3f1270550b90c73
xjccj <1449282308@qq.com>
2023-05-02 19:15:39 +0800

    微调

--- src/main/java/ltd/newbee/mall/common/Constants.java
+++ src/main/java/ltd/newbee/mall/common/Constants.java
@@ -17,6 +17,7 @@ package ltd.newbee.mall.common;
  */
 public class Constants {
     //public final static String FILE_UPLOAD_DIC = "/opt/image/upload/";//上传文件的默认url前缀，根据部署设置自行修改
+
     public final static String FILE_UPLOAD_DIC = "D:\\upload\\";//上传文件的默认url前缀，根据部署设置自行修改
 //    public final static String FILE_UPLOAD_DIC = "/home/larixtools/upload/";//上传文件的默认url前缀，根据部署设置自行修改
 
